de3cd0bf21128246d7a182fa2ccab8ab5caf4b45
maivv-nals <maivv@mynavitechtus.com>
2025-05-17 21:32:38 +0700

    SEO product

--- src/app/(user)/(wrap)/san-pham/[slug]/page.tsx
+++ src/app/(user)/(wrap)/san-pham/[slug]/page.tsx
@@ -1,15 +1,16 @@
 
 import React from "react";
 import Detail from "./Detail";
+import { notFound } from 'next/navigation';
+import { getFileLink } from "@/helper/common";
+import { Metadata } from "next";
 
 type Props = {
   params: { slug: string };
 };
 
-const Page = async ({ params }: Props) => {
-  const cleanSlug = params.slug.replace(/\.html$/, '');
-
-  const data = await fetch(`${process.env.API_BASE_URL}/api/products/${cleanSlug}`, {
+const getDetail = async (slug: string) => {
+  const data = await fetch(`${process.env.API_BASE_URL}/api/products/${slug}`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -17,9 +18,44 @@ const Page = async ({ params }: Props) => {
     },
     cache: 'no-store'
   });
+
+  if (!data.ok) {
+    notFound();
+  }
+
   const response = await data.json()
 
-  const product = response.data;
+  return response.data;
+}
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const cleanSlug = params.slug.replace(/\.html$/, '');
+  const detail = await getDetail(cleanSlug);
+
+  return {
+    title: detail.name,
+    description: detail.name.slice(0, 150),
+    openGraph: {
+      title: detail.name,
+      description: detail.name.slice(0, 150),
+      url: `${process.env.APP_URL}/bai-viet/${detail.slug}.html`,
+      type: 'article',
+      images: [
+        {
+          url: getFileLink(detail.preview_image),
+          width: 800,
+          height: 600,
+          alt: detail.name,
+        },
+      ],
+    },
+  };
+}
+
+const Page = async ({ params }: Props) => {
+  const cleanSlug = params.slug.replace(/\.html$/, '');
+
+  const product = await getDetail(cleanSlug)
 
   return (
     <Detail product={product} />
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <Result
       status="404"
       title="404"
-      subTitle="Sorry, the page you visited does not exist."
+      subTitle="Rất tiếc. Trang này không được tìm thấy."
       extra={
         <Link href="/">
           <Button type="primary">Về trang chủ</Button>
